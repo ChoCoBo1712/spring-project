a3e8b14a57c0acf3a6508870b8c65ab9e6384ddc
ChoCoBo1712 <bigben1712@mail.ru>
2021-10-27 00:13:19 +0300

    add hikariCP datasource

--- repository/src/main/java/com/chocobo/esm/config/DatabaseConfig.java
+++ repository/src/main/java/com/chocobo/esm/config/DatabaseConfig.java
@@ -1,19 +1,24 @@
 package com.chocobo.esm.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DatabaseConfig {
 
+    private static HikariConfig hikariConfig = new HikariConfig();
+
     @Bean
-    public DriverManagerDataSource dataSource() {
-        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-        driverManagerDataSource.setDriverClassName("org.postgresql.Drive");
-        driverManagerDataSource.setUrl("postgres://ddghlknj:90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5@hattie.db.elephantsql.com/ddghlknj");
-        driverManagerDataSource.setUsername("ddghlknj");
-        driverManagerDataSource.setPassword("90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5");
-        return driverManagerDataSource;
+    public DataSource dataSource() {
+        hikariConfig.setDriverClassName("org.postgresql.Drive");
+        hikariConfig.setJdbcUrl( "postgres://ddghlknj:90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5@hattie.db.elephantsql.com/ddghlknj" );
+        hikariConfig.setUsername( "ddghlknj" );
+        hikariConfig.setPassword( "90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5" );
+        hikariConfig.setMaximumPoolSize(5);
+        return new HikariDataSource(hikariConfig);
     }
 }

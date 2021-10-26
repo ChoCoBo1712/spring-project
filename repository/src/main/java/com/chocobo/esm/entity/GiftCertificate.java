4409463739ada90854c93369d62fcbededec5131
ChoCoBo1712 <bigben1712@mail.ru>
2021-10-26 22:20:38 +0300

    tag, gift certificate lombok implementation

--- repository/src/main/java/com/chocobo/esm/entity/GiftCertificate.java
+++ repository/src/main/java/com/chocobo/esm/entity/GiftCertificate.java
@@ -1,4 +1,20 @@
 package com.chocobo.esm.entity;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Data
 public class GiftCertificate {
+
+    private long id;
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private Duration duration;
+    private LocalDateTime createDate;
+    private LocalDateTime lastUpdateDate;
 }
--- repository/src/main/java/com/chocobo/esm/entity/Tag.java
+++ repository/src/main/java/com/chocobo/esm/entity/Tag.java
@@ -1,4 +1,11 @@
 package com.chocobo.esm.entity;
 
+import lombok.Data;
+
+@Data
 public class Tag {
+
+    private long id;
+
+    private String name;
 }

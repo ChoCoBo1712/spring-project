e4f49f4f2162ef6995df61157bf8e1803115da38
ChoCoBo1712 <bigben1712@mail.ru>
2021-10-27 00:13:42 +0300

    add jdbcTemplate injection

--- repository/src/main/java/com/chocobo/esm/repository/impl/GiftCertificateRepositoryImpl.java
+++ repository/src/main/java/com/chocobo/esm/repository/impl/GiftCertificateRepositoryImpl.java
@@ -1,4 +1,19 @@
 package com.chocobo.esm.repository.impl;
 
-public class GiftCertificateRepositoryImpl {
+import com.chocobo.esm.repository.GiftCertificateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class GiftCertificateRepositoryImpl implements GiftCertificateRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 }
--- repository/src/main/java/com/chocobo/esm/repository/impl/TagRepositoryImpl.java
+++ repository/src/main/java/com/chocobo/esm/repository/impl/TagRepositoryImpl.java
@@ -1,4 +1,19 @@
 package com.chocobo.esm.repository.impl;
 
-public class TagRepositoryImpl {
+import com.chocobo.esm.repository.TagRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class TagRepositoryImpl implements TagRepository {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
 }

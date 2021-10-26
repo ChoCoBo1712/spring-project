1d27c93bd5215698ce580e3ef83f1921f254654e
ChoCoBo1712 <bigben1712@mail.ru>
2021-10-26 23:49:19 +0300

    database config initialize

--- repository/src/main/java/com/chocobo/esm/config/DatabaseConfig.java
+++ repository/src/main/java/com/chocobo/esm/config/DatabaseConfig.java
@@ -1,9 +1,19 @@
 package com.chocobo.esm.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 @Configuration
 public class DatabaseConfig {
 
-
+    @Bean
+    public DriverManagerDataSource dataSource() {
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName("org.postgresql.Drive");
+        driverManagerDataSource.setUrl("postgres://ddghlknj:90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5@hattie.db.elephantsql.com/ddghlknj");
+        driverManagerDataSource.setUsername("ddghlknj");
+        driverManagerDataSource.setPassword("90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5");
+        return driverManagerDataSource;
+    }
 }

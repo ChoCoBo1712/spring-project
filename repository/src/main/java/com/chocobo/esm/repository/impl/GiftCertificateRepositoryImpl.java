c5dcc95a8ba3a139cd046a9d37248596486fc36d
ChoCoBo1712 <bigben1712@mail.ru>
2021-10-28 01:11:28 +0300

    start implement repositories

--- repository/src/main/java/com/chocobo/esm/repository/impl/GiftCertificateRepositoryImpl.java
+++ repository/src/main/java/com/chocobo/esm/repository/impl/GiftCertificateRepositoryImpl.java
@@ -1,19 +1,67 @@
 package com.chocobo.esm.repository.impl;
 
+import com.chocobo.esm.entity.GiftCertificate;
+import com.chocobo.esm.entity.Tag;
 import com.chocobo.esm.repository.GiftCertificateRepository;
+import com.chocobo.esm.repository.Ordering;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class GiftCertificateRepositoryImpl implements GiftCertificateRepository {
 
-    private JdbcTemplate jdbcTemplate;
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    private RowMapper<Tag> rowMapper;
 
     @Autowired
     public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Autowired
+    public void setRowMapper(RowMapper<Tag> rowMapper) {
+        this.rowMapper = rowMapper;
+    }
+
+    @Override
+    public List<GiftCertificate> find(long tagId, String certificateName, BigDecimal price, Ordering orderByName, Ordering orderByCreateDate) {
+        return null;
+    }
+
+    @Override
+    public Optional<GiftCertificate> findById(long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void addTag(long certificateId, long tagId) {
+
+    }
+
+    @Override
+    public void removeTag(long certificateId, long tagId) {
+
+    }
+
+    @Override
+    public long create(GiftCertificate certificate) {
+        return 0;
+    }
+
+    @Override
+    public boolean update(GiftCertificate certificate) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(long id) {
+        return false;
     }
 }
--- repository/src/main/java/com/chocobo/esm/repository/impl/TagRepositoryImpl.java
+++ repository/src/main/java/com/chocobo/esm/repository/impl/TagRepositoryImpl.java
@@ -1,19 +1,116 @@
 package com.chocobo.esm.repository.impl;
 
+import com.chocobo.esm.entity.Tag;
 import com.chocobo.esm.repository.TagRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
 
 @Repository
 public class TagRepositoryImpl implements TagRepository {
 
-    private JdbcTemplate jdbcTemplate;
+    private static final String ID_PARAM = "id";
+    private static final String NAME_PARAM = "name";
+    private static final String CERTIFICATE_ID_PARAM = "certificate_id";
+
+    private static final String SELECT_ALL = """
+            SELECT id, name
+            FROM tag;
+            """;
+
+    private static final String SELECT_BY_ID = """
+            SELECT id, name
+            FROM tag
+            WHERE id = :id;
+            """;
+
+    private static final String SELECT_BY_NAME = """
+            SELECT id, name
+            FROM tag
+            WHERE name = :name;
+            """;
+
+    private static final String SELECT_BY_CERTIFICATE_ID = """
+            SELECT tag.id, tag.name
+            FROM tag
+            INNER JOIN certificate_tag ON tag.id = certificate_tag.id_tag
+            WHERE certificate_tag.id_certificate = :certificate_id;
+            """;
+
+    private static final String INSERT = """
+            INSERT INTO tag (name)
+            VALUES (:name);
+            """;
+
+    private static final String DELETE = """
+            DELETE FROM tag
+            WHERE id = :id;
+            """;
+
+    private NamedParameterJdbcTemplate jdbcTemplate;
+    private RowMapper<Tag> rowMapper;
 
     @Autowired
-    public void setDataSource(DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    public void setJdbcTemplate(DataSource dataSource) {
+        this.jdbcTemplate = new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Autowired
+    public void setRowMapper(RowMapper<Tag> rowMapper) {
+        this.rowMapper = rowMapper;
+    }
+
+    @Override
+    public List<Tag> findAll() {
+        return jdbcTemplate.query(SELECT_ALL, rowMapper);
+    }
+
+    @Override
+    public Optional<Tag> findById(long id) {
+        SqlParameterSource parameters = new MapSqlParameterSource().addValue(ID_PARAM, id);
+
+        List<Tag> tags = jdbcTemplate.query(SELECT_BY_ID, parameters, rowMapper);
+        return Optional.ofNullable(tags.size() == 1 ? tags.get(0) : null);
+    }
+
+    @Override
+    public Optional<Tag> findByName(String name) {
+        SqlParameterSource parameters = new MapSqlParameterSource().addValue(NAME_PARAM, name);
+
+        List<Tag> tags = jdbcTemplate.query(SELECT_BY_NAME, parameters, rowMapper);
+        return Optional.ofNullable(tags.size() == 1 ? tags.get(0) : null);
+    }
+
+    @Override
+    public List<Tag> findByCertificateId(long certificateId) {
+        SqlParameterSource parameters = new MapSqlParameterSource().addValue(CERTIFICATE_ID_PARAM, certificateId);
+        return jdbcTemplate.query(SELECT_BY_CERTIFICATE_ID, parameters, rowMapper);
+    }
+
+    @Override
+    public long create(Tag tag) {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        SqlParameterSource parameters = new MapSqlParameterSource().addValue(NAME_PARAM, tag.getName());
+        jdbcTemplate.update(INSERT, parameters, keyHolder);
+
+        Number generatedKey = Objects.requireNonNull(keyHolder).getKey();
+        return Objects.requireNonNull(generatedKey).longValue();
+    }
+
+    @Override
+    public boolean delete(long id) {
+        SqlParameterSource parameters = new MapSqlParameterSource().addValue(ID_PARAM, id);
+
+        return jdbcTemplate.update(DELETE, parameters) > 0;
     }
 }

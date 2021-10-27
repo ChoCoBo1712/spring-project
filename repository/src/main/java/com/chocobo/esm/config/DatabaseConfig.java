1fc08b89e31e34d8adfe0c344750177d4861aca7
ChoCoBo1712 <bigben1712@mail.ru>
2021-10-28 00:14:41 +0300

    add db.properties

--- repository/src/main/java/com/chocobo/esm/config/DatabaseConfig.java
+++ repository/src/main/java/com/chocobo/esm/config/DatabaseConfig.java
@@ -2,23 +2,40 @@ package com.chocobo.esm.config;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource("classpath:db.properties")
 public class DatabaseConfig {
 
-    private static HikariConfig hikariConfig = new HikariConfig();
+    @Value("${db.driver}")
+    private String driverClassName;
+
+    @Value("${db.url}")
+    private String url;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Value("${db.max_pool_size}")
+    private int maxPoolSize;
 
     @Bean
     public DataSource dataSource() {
-        hikariConfig.setDriverClassName("org.postgresql.Drive");
-        hikariConfig.setJdbcUrl( "postgres://ddghlknj:90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5@hattie.db.elephantsql.com/ddghlknj" );
-        hikariConfig.setUsername( "ddghlknj" );
-        hikariConfig.setPassword( "90zioIG8JmI5aHQsg9RWPL6EUVpwoDK5" );
-        hikariConfig.setMaximumPoolSize(5);
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName(driverClassName);
+        hikariConfig.setJdbcUrl(url);
+        hikariConfig.setUsername(username);
+        hikariConfig.setPassword(password);
+        hikariConfig.setMaximumPoolSize(maxPoolSize);
         return new HikariDataSource(hikariConfig);
     }
 }
